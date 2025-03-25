6590cb327c8d01a21d8421792a8e355998d31215
Maximiliano Torreblanca <torreblancamd@gmail.com>
2025-03-25 09:53:28 -0300

    Version 2

--- aulas.sql
+++ aulas.sql
@@ -9,22 +9,7 @@ CREATE TABLE alumnos(
     FOREIGN KEY(id_escuela) REFERENCES escuelas(id)
 
 
-);
-
-CREATE TABLE alumnos(
-	id int(11) NOT NULL AUTO_INCREMENT,
-    id_escuela int(11) DEFAULT NULL,
-    legajo int(11) DEFAULT NULL,
-    nombre varchar(45) DEFAULT NULL,
-    nota decimal(10,0) DEFAULT NULL,
-    email varchar(45) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(id_escuela) REFERENCES escuelas(id)
-
-
-);
-
-CREATE TABLE alumnos(
+);CREATE TABLE alumnos(
 	id int(11) NOT NULL AUTO_INCREMENT,
     id_escuela int(11) DEFAULT NULL,
     legajo int(11) DEFAULT NULL,
